d7e9aaa524a65cfdcfc2dc5c9884131e7fed6f30
James Almeida <james.almeida24@gmail.com>
2017-03-25 13:27:11 -0400

    added Camera view

---
+++ ArToJaThesis/DJICameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  DJICameraViewController.h
+//  ArToJaThesis
+//
+//  Created by James Almeida on 3/25/17.
+//  Copyright © 2017 James Almeida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJICameraViewController : UIViewController
+
+@end
